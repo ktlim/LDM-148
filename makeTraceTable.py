2b229c496b628789cf6c435e0fc21e8eebed3829
Kian-Tat Lim <ktl@slac.stanford.edu>
2018-05-03 20:36:43 -0700

    Remove sort numbers from trace table.

--- makeTraceTable.py
+++ makeTraceTable.py
@@ -6,19 +6,30 @@ import sys
 
 with open(sys.argv[1], "r") as f:
     reader = csv.reader(f)
+
+    # Extract component list from first (header) line
     header = next(reader)
     components = []
     for c in header:
         if "[" in c:
-            component = c[0:c.index("[") - 1]
-            if "&" in component:
-                component = re.sub(r'\&', 'and', component)
+            # Separate component from package
+            component = c.split(" [")[0]
+            # Protect ampersand from LaTeX and make readable
+            component = re.sub(r'\&', 'and', component)
+            # Remove leading digits used for sort ordering
+            component = re.sub(r'^\d+\s+', '', component)
             components.append(component)
     n = len(components)
+
+    # Each succeeding line corresponds to a requirement
     for row in reader:
         req = row[0]
-        req = req[0:req.index("[") - 1]
+        # Separate requirement from package
+        req = req.split(" [")[0]
+        # Protect ampersand from LaTeX and make readable
         req = re.sub(r'\&', 'and', req)
+        # Remove digits after req id used for sort ordering
+        req = re.sub(r'\s+\d+', '', req)
         clist = []
         for i in range(1, n):
             if row[i]:
